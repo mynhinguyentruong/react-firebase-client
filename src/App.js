86296d11c54f991fb1fd2848c2727c8e6e828bbc
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-08-27 11:04:07 -0400

    Basic routes to different pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
 
+import Home from './pages/Home'
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="">
-      hi
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
     </div>
   );
 }
