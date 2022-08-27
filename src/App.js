bfbe3aadecf03b14439fc926be71b227516f6b8f
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-08-27 11:51:10 -0400

    Basic style for navbar

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="">
+    <div className='container'>
+      <Navbar />
       <Routes>
+        
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
+    
       </Routes>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,17 @@
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+
+import { Link } from "react-router-dom"
+
 export default function Navbar() {
   return (
-    <div>
-      Navbar
-    </div>
+    <AppBar>
+      <Toolbar className='center'>
+        <Button color="inherit" component={Link} to='/'>Home</Button>
+        <Button color="inherit" component={Link} to='/login'>Login</Button>
+        <Button color="inherit" component={Link} to='/signup'>Signup</Button>
+      </Toolbar>
+    </AppBar>
   )
 }
