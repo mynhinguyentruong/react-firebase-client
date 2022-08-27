f576ca67f15133669467da533c7a2fa0e26d3dbe
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-08-27 13:59:55 -0400

    Add icon to website

---
+++ src/components/Scream.js
@@ -0,0 +1,8 @@
+export default function Scream() {
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,29 @@
 import Grid from '@material-ui/core/Grid'
+import axios from 'axios'
+
+import { useEffect, useState } from 'react'
+
+import Scream from '../components/Scream'
 
 export default function Home() {
+
+  const [allScreams, setAllScreams] = useState(null)
+
+  // useEffect(() => {
+  //   axios.get('/screams')
+  //     .then(res => {
+  //       console.log(res.data)
+  //       setAllScreams(res.data)
+  //     })
+  //     .catch(err => console.log(err))
+  // }, [])
+
+  let recentScreamsMarkup = allScreams?.map(scream => <Scream key={scream.screamId} />)
+
   return (
-    <Grid container spacing={16}>
+    <Grid container spacing={10}>
       <Grid item sm={8} xs={12}>
-        <p>Content...</p>
+        {recentScreamsMarkup ? recentScreamsMarkup : 'Loading...'}
       </Grid>
       <Grid item sm={4} xs={12} >
         <p>Profile...</p>
