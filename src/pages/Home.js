16d3528ecb8b6ca7cd9532dac7fce88878c9e187
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-08-27 12:14:21 -0400

    Install axios

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
+import Grid from '@material-ui/core/Grid'
+
 export default function Home() {
   return (
-    <div>
-      Home
-    </div>
+    <Grid container spacing={16}>
+      <Grid item sm={8} xs={12}>
+        <p>Content...</p>
+      </Grid>
+      <Grid item sm={4} xs={12} >
+        <p>Profile...</p>
+      </Grid>
+    </Grid>
   )
 }
