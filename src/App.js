d4081854fb86d16c8ae47bd367558905b1e523f9
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-08-29 11:28:06 -0400

    Decode token receive from firebase and check expiration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 
-import Home from './pages/Home'
+import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Navbar from './components/Navbar';
+
+import jwtDecode from 'jwt-decode';
+
+let authenticated;
+
+const token = localStorage.FBIdToken;
+if(token) {
+  const decoded = jwtDecode(token);
+  if(decoded.exp*1000 < Date.now()) {
+    window.location.href = '/login'
+    authenticated = false;
+  } else authenticated = true
+}
 
 function App() {
   return (
