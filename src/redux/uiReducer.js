5524ab883c855f067e4c6aa61c49c02b4f3c85e4
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-08-30 12:35:21 -0400

    Set up UI Reducer logic

--- src/redux/uiReducer.js
+++ src/redux/uiReducer.js
@@ -1,9 +1,34 @@
 // Add and export action
 
-function uiReducer(state, action) {
+const initialState = {
+  loading: false,
+  errors: null
+}
+
+function uiReducer(state = initialState, action) {
   switch(action.type) {
-    case '':
-      return //update state
+    case 'SET_ERRORS':
+      return {
+        ...state,
+        loading: false,
+        errors: action.payload
+      } 
+    case 'CLEAR_ERRORS':
+      return {
+        ...state,
+        loading: false,
+        errors: null
+      }
+    case 'LOADING_UI':
+      return {
+        ...state,
+        loading: true
+      }
+    case 'STOP_LOADING_UI':
+      return {
+        ...state,
+        loading: false
+      }
     default:
       return state
   }
